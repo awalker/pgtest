641e752294826422318958d49cf786ffd6880a41
Adam Walker <awalker@virtualmindset.com>
2020-07-29 14:32:35 -0400

    Support drawing a "room" with the mouse

--- world.gd
+++ world.gd
@@ -1,12 +1,15 @@
 extends Node2D
 class_name CaveWorld
 
+export var level_seed: String
 export (int) var mapWidth := 75
 export (int) var mapHeight := 45
 export (int) var tileSize := 32
 export (int) var fillRatio := 55
 export (int) var maxTime := 2
 export (int) var wallsLimit := 4
+
+var rnd := RandomNumberGenerator.new()
 
 var time := 0
 
@@ -16,11 +19,18 @@ var map := []
 # Dirt is "alive", Walls are "dead"
 enum Tiles { DIRT, WALL, GRASS, VOID }
 
+var makingARoom := false
+var mousePointer := Vector2.ZERO
+var mouseRoomCenter: Vector2
+var mouseRoomEdge: Vector2
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	randomize()
-	# rand_seed(13375)
+	if level_seed:
+		rnd.seed = hash(level_seed)
+	else:
+		rnd.randomize()
 	var w = (mapWidth) * tileSize
 	var h = (mapHeight) * tileSize
 	mapCamera.position = Vector2(w / 2, h / 2)
@@ -32,7 +42,7 @@ func _ready() -> void:
 	createMapAtTimeZero()
 
 
-func _unhandled_input(_event: InputEvent) -> void:
+func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		timeAdvance()
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -61,6 +71,60 @@ func _unhandled_input(_event: InputEvent) -> void:
 		wallsLimit -= 1 * -1 if Input.is_action_pressed("raise_mod") else 1
 		createMapAtTimeZero()
 		timeAdvance()
+	if Input.is_action_just_pressed("clear"):
+		fillRatio = 0
+		createMapAtTimeZero()
+	if event is InputEventMouseMotion:
+		mousePointer = get_global_mouse_position()
+		update()
+	if ! makingARoom && Input.is_mouse_button_pressed(1):
+		print('Starting a room')
+		mouseRoomCenter = get_global_mouse_position()
+		mouseRoomEdge = mouseRoomCenter
+		makingARoom = true
+		update()
+	elif makingARoom && event is InputEventMouse:
+		mouseRoomEdge = get_global_mouse_position()
+		if ! Input.is_mouse_button_pressed(1):
+			makingARoom = false
+			makeARoom(mouseRoomCenter, mouseRoomEdge)
+		update()
+
+
+func _draw():
+	if makingARoom:
+		var radius := 5.0
+		if mouseRoomEdge:
+			radius = mouseRoomCenter.distance_to(mouseRoomEdge)
+		draw_circle(mouseRoomCenter, radius, Color.red)
+	draw_circle(mousePointer, 50.0, Color.green)
+
+
+func makeARoom(center: Vector2, edge: Vector2) -> void:
+	# Create Our Room
+	var maxDistSq := center.distance_squared_to(edge)
+	var maxDist := center.distance_to(edge) / tileSize
+	var cx := round(center.x / tileSize) as int
+	var cy := round(center.y / tileSize) as int
+	var tl := Vector2(cx - maxDist, cy - maxDist)
+	for _y in range(tl.y, tl.y + maxDist * 2):
+		for _x in range(tl.x, tl.x + maxDist * 2):
+			var x: int = clamp(_x, 0, mapWidth - 1) as int
+			var y: int = clamp(_y, 0, mapHeight - 1) as int
+			var tile: int = map[x][y]
+			var percent := (
+				1.0
+				- clamp(
+					center.distance_squared_to(Vector2(x * tileSize, y * tileSize)) / maxDistSq,
+					0.0,
+					1.0
+				)
+			)
+			print(percent)
+			if rnd.randf() < percent:
+				tile = Tiles.DIRT
+			map[x][y] = tile
+	mapToTileMap()
 
 
 func updateUI() -> void:
@@ -111,7 +175,7 @@ func createMapAtTimeZero() -> void:
 		for y in range(0, mapHeight):
 			var tile: int = Tiles.WALL
 			if x > 0 && x < mapWidth - 1 && y > 0 && y < mapHeight - 1:
-				var r = (randi() % 100) + 1
+				var r = rnd.randi_range(0, 100)
 				if r < fillRatio:
 					tile = Tiles.DIRT
 				else:
